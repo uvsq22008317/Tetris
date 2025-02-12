1bbd7fb5817bbebeeae16211fdfbf551a547176c
Jonathan Long <jonathan.long@ens.uvsq.fr>
2025-02-12 14:39:18 +0100

    fix nb color, 1 pièce non affiché

--- tetris/src/Logic_game/Tetris_game.js
+++ tetris/src/Logic_game/Tetris_game.js
@@ -14,7 +14,7 @@ function TetrisGame() {
 
     let grid = Array.from({ length: GRID_ROWS }, () => Array(GRID_COLUMNS).fill(0));
 
-    const colors = [0xff0000, 0x00ff00, 0x0000ff, 0xffff00, 0xff00ff, 0x00ffff];
+    const colors = [0xff0000, 0x00ff00, 0x0000ff, 0xffff00, 0xff00ff, 0x00ffff, 0xffffff];
 
     const shapes = [
         // O (square)
