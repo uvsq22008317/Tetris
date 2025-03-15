2ac22dfa5892021fc02c9c58800a13190aedc72f
Lina CHENNI <lina.dz@orange.fr>
2025-03-15 20:23:16 +0100

    correction multijoueur

--- src/client/src/socket.js
+++ src/client/src/socket.js
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000/");
+const socket = io(process.env.REACT_APP_API_URL, {
+    transports: ["websocket"],
+  });  
 
 export default socket;
