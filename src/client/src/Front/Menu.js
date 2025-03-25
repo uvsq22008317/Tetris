82463d627bfe5867304fe6c508eef57754fa5bcf
Jonathan Long <jonathan.long@ens.uvsq.fr>
2025-03-25 23:32:22 +0100

    format time in profil

--- src/client/src/Front/Menu.js
+++ src/client/src/Front/Menu.js
@@ -10,6 +10,13 @@ function Menu({ changepage, setIsLoggedIn }) {
     setisPageOpen(!isPageOpen);
   };
 
+  function timeFormat(time) {
+    let minutes = Math.floor(time / 60000);
+    let seconds = Math.floor((time % 60000) / 1000);
+    let milliseconds = (time % 1000).toFixed(0);
+    return `${minutes}:${(seconds < 10 ? "0" : "")}${seconds},${milliseconds}`;
+  }
+
   useEffect(() => {
     if (isPageOpen) {
       const username = localStorage.getItem("username");
@@ -84,13 +91,13 @@ function Menu({ changepage, setIsLoggedIn }) {
               <>
                 <p>Nom d'utilisateur : {userData.username}</p>
                 <h2>Mode 40L</h2>
-                <p>Highscore : {getScore(userData.highscore40L)}</p>
+                <p>Highscore : {timeFormat(getScore(userData.highscore40L))}</p>
                 <h2>Mode Ultra</h2>
                 <p>Highscore : {getScore(userData.ultraHighscore)}</p>
                 <h2>Mode Cheese</h2>
-                <p>Highscore : {getScore(userData.cheeseHighscore)}</p>
+                <p>Highscore : {timeFormat(getScore(userData.cheeseHighscore))}</p>
                 <h2>Mode Rush</h2>
-                <p>Highscore : {getScore(userData.rushHighscore)}</p>
+                <p>Highscore : {timeFormat(getScore(userData.rushHighscore))}</p>
               </>
             )}
             <button onClick={handleLogout}>Deconnexion</button>
